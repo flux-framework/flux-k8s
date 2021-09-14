33f39be5e338aa4d828b9ae6a7a478b59bab5be3
Maurizio Drocco <maurizio.drocco@ibm.com>
2021-09-14 09:20:41 -0400

    cleanup

--- examples/gromacs/run-all.sh
+++ examples/gromacs/run-all.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+SCHEDULER_MANIFEST=$(mktemp)
+
 echo Running default scheduler
 ./launch.sh default default
 
 echo Running KubeFlux scheduler
-sed s/%TAG%/latest/g  kubesched.template.yaml > kubesched.yaml
-./launch flux flux kubesched.yaml
+sed s/%TAG%/latest/g  kubesched.template.yaml > ${SCHEDULER_MANIFEST}
+./launch flux flux ${SCHEDULER_MANIFEST}
 
 echo Running KubeFlux scheduler with subnet awareness
-sed s/%TAG%/sa/g  kubesched.template.yaml > kubesched.yaml
-./launch flux flux kubesched.yaml
+sed s/%TAG%/sa/g  kubesched.template.yaml > ${SCHEDULER_MANIFEST}
+./launch flux flux ${SCHEDULER_MANIFEST}
+
+rm ${SCHEDULER_MANIFEST}
