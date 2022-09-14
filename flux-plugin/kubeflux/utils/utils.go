8ea256466aed01fdc3208df0e20166bcdc2a42c1
cmisale <c.misale@ibm.com>
2022-09-14 14:25:18 +0000

    fixes to packages paths

--- flux-plugin/kubeflux/utils/utils.go
+++ flux-plugin/kubeflux/utils/utils.go
@@ -6,7 +6,7 @@ import (
 	// "strings"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
-	"kubeflux/jgf"
+	"github.com/flux-framework/flux-k8s/flux-plugin/kubeflux/jgf"
 	"encoding/json"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/kubernetes"
